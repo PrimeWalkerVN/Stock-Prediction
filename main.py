11e91ea84bb9fabb696660fd0808157342c1f71f
shivaluma <nchithanh609@gmail.com>
2021-08-16 22:52:42 +0700

    Train model

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ from ta.momentum import RSIIndicator
 from ta.momentum import ROCIndicator
 from io import BytesIO
 import base64
-
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+import math
 ###########
 # sidebar #
 ###########
@@ -33,8 +37,83 @@ else:
 # Train model #
 #############
 
+#For LSTM MOdel ------------------------------
 
-# Code train model here
+
+def create_train_test_LSTM(df, epoch, b_s, ticker_name):
+    df_filtered = df.filter(['Close'])
+    dataset = df_filtered.values
+
+    #Training Data
+    training_data_len = math.ceil(len(dataset) * .7)
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(dataset)
+
+    train_data = scaled_data[0: training_data_len, :]
+
+    x_train_data, y_train_data = [], []
+
+    for i in range(60, len(train_data)):
+        x_train_data.append(train_data[i-60:i, 0])
+        y_train_data.append(train_data[i, 0])
+
+    x_train_data, y_train_data = np.array(x_train_data), np.array(y_train_data)
+
+    x_train_data = np.reshape(
+        x_train_data, (x_train_data.shape[0], x_train_data.shape[1], 1))
+
+    #Testing Data
+    test_data = scaled_data[training_data_len - 60:, :]
+
+    x_test_data = []
+    y_test_data = dataset[training_data_len:, :]
+
+    for j in range(60, len(test_data)):
+        x_test_data.append(test_data[j - 60:j, 0])
+
+    x_test_data = np.array(x_test_data)
+
+    x_test_data = np.reshape(
+        x_test_data, (x_test_data.shape[0], x_test_data.shape[1], 1))
+
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True,
+              input_shape=(x_train_data.shape[1], 1)))
+    model.add(LSTM(units=50, return_sequences=False))
+
+    model.add(Dense(25))
+    model.add(Dense(1))
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    model.fit(x_train_data, y_train_data,
+              batch_size=int(b_s), epochs=int(epoch))
+    st.success("Your Model is Trained Succesfully!")
+    st.markdown('')
+    st.write("Predicted vs Actual Results for LSTM")
+    st.write("Stock Prediction on Test Data for - ", ticker_name)
+
+    predictions = model.predict(x_test_data)
+    predictions = scaler.inverse_transform(predictions)
+
+    train = df_filtered[:training_data_len]
+    valid = df_filtered[training_data_len:]
+    valid['Predictions'] = predictions
+
+    new_valid = valid.reset_index()
+    new_valid.drop('index', inplace=True, axis=1)
+    st.dataframe(new_valid)
+    st.markdown('')
+    st.write("Plotting Actual vs Predicted ")
+
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    plt.figure(figsize=(14, 8))
+    plt.title('Actual Close prices vs Predicted Using LSTM Model', fontsize=20)
+    plt.plot(valid[['Close', 'Predictions']])
+    plt.legend(['Actual', 'Predictions'], loc='upper left', prop={"size": 20})
+    st.pyplot()
+
 
 
 ##############
@@ -43,6 +122,9 @@ else:
 
 # Download data
 df = yf.download(user_input,start= start_date,end= datetime.datetime.combine(end_date, end_time), progress=False)
+df = df.reset_index()
+df['Date'] = pd.to_datetime(df['Date']).dt.date
+create_train_test_LSTM(df, 1, 1, user_input)
 
 # Price of change 
 roc = ROCIndicator(df['Close']).roc()
